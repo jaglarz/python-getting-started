04973fe39ab484ddbac628e1756ea26b8b4f1471
jaglarz <155383032+jaglarz@users.noreply.github.com>
2026-01-06 22:03:00 +0100

    Update models.py
    Signed-off-by: jaglarz <155383032+jaglarz@users.noreply.github.com>

--- hello/models.py
+++ hello/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Visit(models.Model): 
-created_at = models.DateTimeField(auto_now_add=True) 
+    created_at = models.DateTimeField(auto_now_add=True) 
 
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
