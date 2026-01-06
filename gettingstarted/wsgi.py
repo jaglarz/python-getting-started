baa3f7860dc299ff87f2d1d6728c93648927e7e9
jaglarz <155383032+jaglarz@users.noreply.github.com>
2026-01-06 23:19:09 +0100

    Update wsgi.py
    Signed-off-by: jaglarz <155383032+jaglarz@users.noreply.github.com>

--- gettingstarted/wsgi.py
+++ gettingstarted/wsgi.py
@@ -1,16 +1,17 @@
-"""
-WSGI config for gettingstarted project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+import django
+from django.core.management import call_command
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
+
+# Inicjalizacja Django
+django.setup()
+
+# Automatyczne migracje przy starcie
+try:
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print("Błąd przy migracjach:", e)
 
 application = get_wsgi_application()
